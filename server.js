e010ab288682bfbbe07f5d86512efc28dcf64a0a
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-02 15:37:07 +0100

    create endpoint for one order w/ id

--- server.js
+++ server.js
@@ -13,16 +13,34 @@ const port = PORT;
 /* get all orders */
 app.get("/orders/", async (req, res) => {
   try {
-    const data = await dbConnection.query("SELECT * FROM orders");
+    const orders = await dbConnection.query("SELECT * FROM orders");
     res.json({
       code: 200,
       operation: "success",
       description: "Fetched all orders",
-      data: data.rows,
+      data: orders.rows,
     });
   } catch (e) {
     console.error(Error(e));
-    res.status(500).send("Something happened");
+    res.status(500).send("ERROR occurred - no Orders for you today");
+  }
+});
+
+/* get all orders */
+app.get("/orders/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const order = await dbConnection.query("SELECT * FROM orders WHERE id=$1", [id]);
+    res.json({
+      code: 200,
+      operation: "success",
+      description: "Fetched all orders",
+      data: order.rows,
+    });
+  } catch (e) {
+    console.error(Error(e));
+    res.status(500).send("ERROR occurred - no Orders for you today");
   }
 });
 
