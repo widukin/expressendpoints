ea897e02a17f26925854806b8478789aeec4f457
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-04 11:41:34 +0100

    add post route

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = express.Router();
+const UserRouter = express.Router();
 const usersControllers = require("../controllers/usersControllers");
 
 
-router.get("/:id", usersControllers.getById);
-router.get("/", usersControllers.getAll);
-/* router.post("/user", usersControllers.create); */
+UserRouter.get("/:id", usersControllers.getById);
+UserRouter.get("/", usersControllers.getAll);
+UserRouter.post("/", usersControllers.create);
 
-module.exports = router;
+module.exports = UserRouter;
