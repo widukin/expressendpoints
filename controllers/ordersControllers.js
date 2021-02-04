c18d3351127eb2fe0612a7ec8d9a13dee5d0b970
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-04 12:04:42 +0100

    apply json_build_object to create nice data object

--- controllers/ordersControllers.js
+++ controllers/ordersControllers.js
@@ -3,7 +3,22 @@ const dbConnection = require("../db.config");
 module.exports = {
   getAll: async (req, res) => {
     try {
-      const orders = await dbConnection.query("SELECT * FROM orders");
+      const orders = await dbConnection.query(
+        `SELECT 
+          json_build_object(
+            'id', orders.id,
+            'price', orders.price,
+            'date', orders.date,
+            'user_id', orders.user_id,
+            'user', json_build_object(
+              'id', users.id,
+              'first_name', users.first_name,
+              'last_name', users.last_name,
+              'age', users.age
+            )
+          ) 
+        FROM orders 
+        JOIN users ON users.id = orders.user_id`);
       res.json({
         code: 200,
         operation: "success",
@@ -19,12 +34,29 @@ module.exports = {
     try {
       const { id } = req.params;
   
-      const order = await dbConnection.query("SELECT * FROM orders WHERE id=$1", [id]);
+      const order = await dbConnection.query(
+        `SELECT 
+          json_build_object(
+            'id', orders.id,
+            'price', orders.price,
+            'date', orders.date,
+            'user_id', orders.user_id,
+            'user', json_build_object(
+              'id', users.id,
+              'first_name', users.first_name,
+              'last_name', users.last_name,
+              'age', users.age
+            )
+          ) 
+        FROM orders 
+        JOIN users ON users.id = orders.user_id 
+        WHERE orders.id=$1`, 
+      [id]);
       res.json({
         code: 200,
         operation: "success",
         description: "Fetched all orders",
-        data: order.row,
+        data: order.rows,
       });
     } catch (e) {
       console.error(Error(e));
