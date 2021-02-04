008d11ad1d6abaf35f98d9bd7ef6a5cdcebd0abb
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-04 12:29:26 +0100

    implement funcionality to insert new user

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -18,7 +18,7 @@ module.exports = {
   getById: async (req, res) => {
     try {
       const { id } = req.params;
-      const dbResponse = await dbConnection.query("SELECT * FROM orders WHERE id=$1", [id]);
+      const dbResponse = await dbConnection.query("SELECT * FROM users WHERE id=$1", [id]);
       res.json({
         code: 200,
         operation: "success",
@@ -26,10 +26,42 @@ module.exports = {
         data: dbResponse.rows,
       });
     } catch (e) {
-      console.log("here");
-      console.error("h2", Error(e));
+      console.error(Error(e));
       res.status(500).send("ERROR occurred - no User for you today");
     }
   },
-  /* create: async () */
+  create: async (req, res) => {
+    console.log(req.body);
+    const { first_name, last_name, age } = req.body;
+    // validation on the fields
+    // if statement to check that all 3 exist, that they are not empty
+    if (!first_name.trim().length || !last_name.trim().length || !age) {
+      res.status(400).json({
+        error: "Fields are invalid",
+        code: 400,
+      });
+      return;
+    }
+    try {
+      const dbResponse = await dbConnection.query(`INSERT INTO "users" (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *;`, [
+        first_name,
+        last_name,
+        age,
+      ]);
+      res.json({
+        code: 200,
+        operation: "success",
+        description: "Inserted user",
+        data: dbResponse.rows[0],
+      })
+    } catch (error) {
+      console.error(Error(e));
+      res.status(500)
+        .json({
+          code: 500,
+          message: "Error trying to insert a new user"
+        })
+        .send("ERROR occurred - no User for you today");
+    }
+  }
 }
