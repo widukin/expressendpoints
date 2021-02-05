bf1fb017501169a235be56591962b830cbe63ec9
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-05 15:38:38 +0100

    implement get profile picture middleware

--- controllers/uploadControllers.js
+++ controllers/uploadControllers.js
@@ -1,2 +1,18 @@
 // import Multer (a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.)
 const multer = require("multer");
+
+module.exports = {
+  getProfilPic: async (req, res, next) => {
+    try {
+      console.log(req.file);
+      res.send(`file ${req.file.filename} uploaded`);
+    } catch (e) {
+      console.error(Error(e));
+      res.sendStatus(500).send("ERROR occurred - no Upload");
+    }
+  },
+  // do I need that, works also without
+  getIndex: async (req, res) => {
+    res.sendFile("public/index.html");
+  },
+};
