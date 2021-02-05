07187d3de9753f2e2b4b1802ae75bc65e3c92d92
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-05 15:37:37 +0100

    chaned to multer diskStorage to define filename

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const path = require("path");
 const UploadRouter = express.Router();
 const uploadControllers = require("../controllers/uploadControllers");
 // import Multer (a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.)
 const multer = require("multer");
+
 // define upload destination storage directory
-var upload = multer({ dest: "public/uploads/" });
+/* var upload = multer({ dest: "public/uploads/" }); */
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "./public/uploads/");
+  },
+  filename: (_, file, cb) => {
+    cb(
+      null,
+      //`${file.fieldname} - ${Date.now()}.$f{ile.mimetype.split("/")[1]}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+const upload = multer({ storage: storage });
 
 UploadRouter.get("/", uploadControllers.getIndex);
 
