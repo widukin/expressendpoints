f656417f496d888f8fb96ce3a6de75bff866a136
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-02-05 14:00:07 +0100

    include multer and routes for get Index and upload profile Picture

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const UploadRouter = express.Router();
 const uploadControllers = require("../controllers/uploadControllers");
+// import Multer (a node.js middleware for handling multipart/form-data, which is primarily used for uploading files.)
+const multer = require("multer");
+// define upload destination storage directory
+var upload = multer({ dest: "public/uploads/" });
 
-UploadRouter.post("/upload-profile-pic", uploadControllers.getProfilPic);
+UploadRouter.get("/", uploadControllers.getIndex);
+
+UploadRouter.post(
+  "/upload-profile-pic",
+  //profile_pic is the name of the input type file
+  upload.single("profile_pic"),
+  uploadControllers.getProfilPic
+);
 
 module.exports = UploadRouter;
